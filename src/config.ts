ac0f3efcbeefbf9b23f84e73dcfb12714ca05751
vouts96 <bivos96@gmail.com>
2025-02-19 21:31:08 +0200

    edit config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://taxisnet.dslab.ece.ntua.gr/api' // Centralized API URL
+    apiUrl: 'http://localhost:8080/api' // Centralized API URL
   };
   
